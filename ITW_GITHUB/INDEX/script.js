93640c5bf5f6637e9d3b4856667b5342a5b60bea
bagz-del <bastiangomezsuarez@gmail.com>
2025-10-20 19:17:34 -0300

    Index modificado: añadido botón registro y pantallas de información

--- ITW_GITHUB/INDEX/script.js
+++ ITW_GITHUB/INDEX/script.js
@@ -1,7 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const loginBtn = document.getElementById("loginBtn");
+  const registerBtn = document.getElementById("registerBtn");
 
   loginBtn.addEventListener("click", () => {
     window.location.href = "../LOGIN/login.html";
+  });
+
+  registerBtn.addEventListener("click", () => {
+    window.location.href = "../REGISTRO/registro.html";
+  });
+
+  const contactForm = document.getElementById("contactForm");
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("✅ ¡Gracias por tu mensaje! Nos pondremos en contacto pronto.");
+    contactForm.reset();
   });
 });
